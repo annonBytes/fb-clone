800872a609971e9288523654286f94fb4778034f
Daniel Ockiya <annonbytes@gmail.com>
2021-02-25 14:55:17 +0100

    Responsive header

--- fb-clone/src/components/Header.jsx
+++ fb-clone/src/components/Header.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import '../components/Header.css'
 import logo from '../assets/facebook.svg'
-import SearchIcon from '@material-ui/icons/Search';
+import SearchIcon from '@material-ui/icons/Search'
+import HomeIcon from '@material-ui/icons/Home'
+import FlagIcon from '@material-ui/icons/Flag'
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions'
+import StorefrontIcon from '@material-ui/icons/Storefront'
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle'
+import AddIcon from '@material-ui/icons/Add'
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive'
+import ForumIcon from '@material-ui/icons/Forum'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import { Avatar, IconButton } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+
 
 function Header() {
     return (
@@ -13,15 +25,54 @@ function Header() {
 
             <div className="header__input">
                <SearchIcon />
-               <input type="text"/>
+               <input type="text" placeholder="search facebook" aria-autocomplete="list" />
             </div>
+                
 
-            <div className="header__middle">
+            <div className="header__center">
+                <div className="header__option header__option--active">
+                   <HomeIcon fontSize="large" />
+                </div>
 
+                <div className="header__option">
+                   <FlagIcon fontSize="large" />
+                </div>
+               
+                <div className="header__option">
+                   <SubscriptionsIcon fontSize="large" />
+                </div>
+               
+                <div className="header__option">
+                   <StorefrontIcon fontSize="large" />
+                </div>
+
+                <div className="header__option">
+                   <SupervisedUserCircleIcon fontSize="large" />
+                </div>
             </div>
 
             <div className="header__right">
+                <div className="header__info">
+                    <Avatar src="" />
+                    <h4>Daniel Ockiya</h4>
+                </div>
                 
+                <IconButton>
+                    <AddIcon />
+                </IconButton>
+
+                <IconButton>
+                    <ForumIcon />
+                </IconButton>
+
+                <IconButton>
+                    <NotificationsActiveIcon />
+                </IconButton>
+
+                <IconButton>
+                    <ExpandMoreIcon />
+                </IconButton>
+
             </div>
         </div>
     )
