b33ce324904154ab044ed682a78117bbf8278025
Daniel Ockiya <annonbytes@gmail.com>
2021-03-01 22:27:21 +0100

    post styling

--- fb-clone/src/components/Feed.jsx
+++ fb-clone/src/components/Feed.jsx
@@ -9,7 +9,25 @@ function Feed() {
         <div className="feed">
             <StoryReel />
             <MessageSender />
-            <Post />
+            <Post 
+               key={1}
+               profilePic="https://i.pinimg.com/474x/7b/07/70/7b07705f9e03ee9db3289e652eb5e1ed.jpg"
+               message="Welcome to BTJ"
+               timestamp="this is a timestamp"
+               username="@bytesOvCode"
+               image="https://images.unsplash.com/photo-1611095969382-19fbe23cebff?ixid=MXwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60"
+            />
+              
+              <Post 
+               key={1}
+               profilePic="https://i.pinimg.com/474x/7b/07/70/7b07705f9e03ee9db3289e652eb5e1ed.jpg"
+               message="Welcome to BTJ"
+               timestamp="this is a timestamp"
+               username="@bytesOvCode"
+               image="https://images.unsplash.com/photo-1614609953905-baeff400aab3?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60"
+            />
+            
+
         </div>
     )
 }
--- fb-clone/src/components/Post.jsx
+++ fb-clone/src/components/Post.jsx
@@ -2,15 +2,25 @@ import { Avatar } from '@material-ui/core'
 import React from 'react'
 import '../components/Post.css'
 
-function Post({profilePic, image, username, timestamp, message}) {
+function Post({id, profilePic, image, username, timestamp, message}) {
     return (
         <div className="post">
              <div className="post__top">
                  <Avatar src={profilePic} className="post__avatar" />
+                
                  <div className="post__topInfo">
                      <h3>{username}</h3>
-                     <p>Timestamp...</p>
+                     <p>{timestamp}</p>
                  </div>
+
+             </div>
+
+             <div className="post__bottom">
+                     <p>{message}</p>
+                 </div>
+
+             <div className="post__image">
+                 <img src={image} alt=""/>
              </div>
         </div>
     )
